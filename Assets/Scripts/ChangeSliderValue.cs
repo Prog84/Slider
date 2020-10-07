62f46492bf4914fa9dabd450ed307f0eda7ff2b4
Prog84 <bvv198424@rambler.ru>
2020-10-07 11:01:01 +1000

    Initial commit

---
+++ Assets/Scripts/ChangeSliderValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ChangeSliderValue : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _duration;
+    public void ChangeValue(float healthValue)
+    {
+        _slider.DOValue(_slider.value + healthValue, _duration);
+    }
+}
